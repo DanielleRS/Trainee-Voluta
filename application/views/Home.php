51ef30f17278de535b3e687d2201aa7da0e8a039
Danielle Rodrigues <30360153+DanielleRS@users.noreply.github.com>
2018-11-12 15:05:42 -0200

    Ajuste conforme prototipo

--- application/views/Home.php
+++ application/views/Home.php
@@ -1,20 +1,14 @@
 <header class="masthead text-center text-white d-flex">
-  <div class="container my-auto">
-    <div class="row">
-      <div class="col-lg-10 mx-auto">
         <img style="width: 100%; padding: 50px" src="<?php echo base_url('assets/img/banner.jpeg');?>">
-        <hr class="light">
-      </div>
-    </div>
-  </div>
+        <hr>
 </header>
-
+<br/><br/><br/>
 <section  id="sobre">
   <div class="container">
     <div class="row">
       <div class="col-lg-8 mx-auto text-center">
-        <img src="img/logo/perfil.png" alt="Empresa Jr" align="center" width="150px" class="circulo" />
-        <hr class="my-4">
+        <img src="<?php echo base_url('assets/img/logo.png');?>" align="center" width="150px" class="circulo"/>
+        <br/><br/>
         <h4>Sobre</h4>
         <p class="mb-4">
             A [nome da empresa jr] é uma associação sem fins lucrativos composta e gerida totalmente por alunos 
@@ -23,25 +17,34 @@
             aos nossos clientes, sempre de forma prazerosa e com a certeza de que um bom profissional é formado por meio de 
             experiências sólidas.
         </p>
+        <br/>
+        <div class="col-md-12">
+          <div class="row">
+            <div class="col-md-4">
+              <h4 class="text-center">Missão</h4>
+              <p class="mb-4 text-center">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a justo dolor. Curabitur blandit sem at magna finibus rutrum. Suspendisse nisl eros, luctus vitae justo ac, vestibulum rhoncus erat. Nullam ac accumsan justo. 
+              Pellentesque hendrerit est scelerisque tincidunt blandit. Sed at est quis tellus tempor mollis.
+              </p>
+            </div>
 
-        <h4>Missão</h4>
-        <p class="mb-4">
-            A [nome da empresa jr] é uma associação sem fins lucrativos composta e gerida totalmente por alunos 
-            de graduação do curso de [curso] da [nome da universidade]. Há [?] anos no mercado, a [nome da empresa] 
-            vem desenvolvendo e oferecendo soluções nas áreas de [...citar áreas]. Buscamos oferecer projetos de qualidade 
-            aos nossos clientes, sempre de forma prazerosa e com a certeza de que um bom profissional é formado por meio de 
-            experiências sólidas.
-        </p>
+            <div class="col-md-4">
+              <h4 class="text-center">Visão</h4>
+              <p class="mb-4 text-center">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a justo dolor. Curabitur blandit sem at magna finibus rutrum. Suspendisse nisl eros, luctus vitae justo ac, vestibulum rhoncus erat. Nullam ac accumsan justo. 
+              Pellentesque hendrerit est scelerisque tincidunt blandit. Sed at est quis tellus tempor mollis.
+              </p>
+            </div>
 
-        <h4>Valores</h4>
-        <p class="mb-4">
-            A [nome da empresa jr] é uma associação sem fins lucrativos composta e gerida totalmente por alunos 
-            de graduação do curso de [curso] da [nome da universidade]. Há [?] anos no mercado, a [nome da empresa] 
-            vem desenvolvendo e oferecendo soluções nas áreas de [...citar áreas]. Buscamos oferecer projetos de qualidade 
-            aos nossos clientes, sempre de forma prazerosa e com a certeza de que um bom profissional é formado por meio de 
-            experiências sólidas.
-        </p>
-
+            <div class="col-md-4">
+              <h4 class="text-center">Valores</h4>
+              <p class="mb-4 text-center">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a justo dolor. Curabitur blandit sem at magna finibus rutrum. Suspendisse nisl eros, luctus vitae justo ac, vestibulum rhoncus erat. Nullam ac accumsan justo. 
+              Pellentesque hendrerit est scelerisque tincidunt blandit. Sed at est quis tellus tempor mollis.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -49,13 +52,36 @@
 
 <section  id="servicos">
   <div class="container">
-    <div class="row">
-      <div class="col-lg-8 mx-auto text-center">
-        <h4>Serviços</h4>
-        <p class="mb-4">
-          Veja como podemos realizar o seu projeto!
-        </p>
-
+    <h4 class="text-center">Serviços oferecidos pela empresa</h4>  
+    <br/>
+    <div class="col-md-12">
+      <div class="row">
+        <div class="col-md-4">
+          <img src="<?php echo base_url('assets/img/servicos/servico1.jpeg');?>" align="center" width="350px" class="circulo"/>
+          <p class="text-center">Serviço 1</p>
+        </div>
+        <div class="col-md-4">
+          <img src="<?php echo base_url('assets/img/servicos/servico2.jpeg');?>" align="center" width="350px" class="circulo"/>
+          <p class="text-center">Serviço 2</p>
+        </div>
+        <div class="col-md-4">
+          <img src="<?php echo base_url('assets/img/servicos/servico3.jpeg');?>" align="center" width="350px" class="circulo"/>
+          <p class="text-center">Serviço 3</p>
+        </div>
+      </div>
+    </div>
+        <br/>
+    <div class="col-md-12">
+      <div class="row">
+        <div class="col-md-4">
+          <img src="<?php echo base_url('assets/img/servicos/servico4.jpeg');?>" align="center" width="350px" class="circulo"/>
+          <p class="text-center">Serviço 4</p>
+        </div>
+        <div class="col-md-4">
+          <img src="<?php echo base_url('assets/img/servicos/servico5.jpeg');?>" align="center" width="350px" class="circulo"/>
+          <p class="text-center">Serviço 5</p>
+        </div>
+        <div class="col-md-4"></div>
       </div>
     </div>
   </div>
@@ -63,13 +89,51 @@
 
 <section  id="portfolio">
   <div class="container">
-    <div class="row">
-      <div class="col-lg-8 mx-auto text-center">
-        <h4>Portfólio</h4>
-        <p class="mb-4">
-          Veja os projetos que já realizamos!
-        </p>
-
+    <h4 class="text-center">Portfólio</h4>
+    <br/>
+    <div class="col-md-12">
+      <div class="row">
+        <div class="col-md-4">
+          <img src="<?php echo base_url('assets/img/portfolio/port1.jpeg');?>" align="center" width="350px" class="circulo"/>
+          <p class="text-center">Título do projeto</p>
+          <p class="text-center">Descrição do projeto</p>
+          <p class="text-center">Características gerais do projeto</p>
+        </div>
+        <div class="col-md-4">
+          <img src="<?php echo base_url('assets/img/portfolio/port2.jpeg');?>" align="center" width="350px" height="321.83" class="circulo"/>
+          <p class="text-center">Título do projeto</p>
+          <p class="text-center">Descrição do projeto</p>
+          <p class="text-center">Características gerais do projeto</p>
+        </div>
+        <div class="col-md-4">
+          <img src="<?php echo base_url('assets/img/portfolio/port3.jpeg');?>" align="center" width="350px" height="321.83" class="circulo"/>
+          <p class="text-center">Título do projeto</p>
+          <p class="text-center">Descrição do projeto</p>
+          <p class="text-center">Características gerais do projeto</p>
+        </div>
+      </div>
+    </div>
+    <br/>
+    <div class="col-md-12">
+      <div class="row">
+        <div class="col-md-4">
+          <img src="<?php echo base_url('assets/img/portfolio/port4.jpeg');?>" align="center" width="350px" height="321.83" class="circulo"/>
+          <p class="text-center">Título do projeto</p>
+          <p class="text-center">Descrição do projeto</p>
+          <p class="text-center">Características gerais do projeto</p>
+        </div>
+        <div class="col-md-4">
+          <img src="<?php echo base_url('assets/img/portfolio/port5.jpeg');?>" align="center" width="350px" height="321.83" class="circulo"/>
+          <p class="text-center">Título do projeto</p>
+          <p class="text-center">Descrição do projeto</p>
+          <p class="text-center">Características gerais do projeto</p>
+        </div>
+        <div class="col-md-4">
+          <img src="<?php echo base_url('assets/img/portfolio/port6.jpeg');?>" align="center" width="350px" height="321.83" class="circulo"/>
+          <p class="text-center">Título do projeto</p>
+          <p class="text-center">Descrição do projeto</p>
+          <p class="text-center">Características gerais do projeto</p>
+        </div>
       </div>
     </div>
   </div>
--- application/views/template/Footer.php
+++ application/views/template/Footer.php
@@ -4,20 +4,21 @@
         <footer class="rodape" id="footer">
             <div class="col-md-12 mx-auto text-center">
                 <h4>Contato</h4>
+                <br/>
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-4">
+                        <div class="col-md-6">
                             <div class="form-group">
                                 <input
                                     class="form-control" 
                                     placeholder="Nome" 
-                                    autofocus>
+                                >
                             </div>
                             <div class="form-group">
                             <input
                                 class="form-control" 
                                 placeholder="E-mail" 
-                                autofocus>
+                            >
                             </div>
                             <div class="form-group">
                                 <textarea 
@@ -25,15 +26,17 @@
                                     placeholder="Mensagem">
                                 </textarea>
                             </div>
+                            <button type="submit" class="btn btn-primary">Enviar</button>
                         
                         </div>
-                        <div class="col-md-4">
-                            <p>Endereço</p>
-                            <p>Horário de atendimento</p>
-                            <p>Telefone</p>
-                        </div>
-                        <div class="col-md-4">
-                            <p>Mapa</p>
+                        <div class="col-md-6">
+                            <p><b>Endereço:</b> Rua XYZ, 11, Bairro, Cidade</p>
+                            <p><b>Horário de atendimento:</b> De 8h às 18h</p>
+                            <p><b>Tel:</b> (31)1111-1111</p>
+
+                            <i class="fa fa-2x fa-facebook" style="margin-right: 10px"></i>
+                            <i class="fa fa-2x fa-instagram" style="margin-right: 10px"></i>
+                            <i class="fa fa-2x fa-linkedin"></i>
                         </div>
                     </div>
                 </div>
